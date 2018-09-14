3f45b5abfaf25ab79fcb67d02a4e08354ee49b38
quanghienle <Hien1308>
2018-09-13 23:24:15 -0400

    few updates on player test

--- src/test/java/simpleBlackJack/PlayerTest.java
+++ src/test/java/simpleBlackJack/PlayerTest.java
@@ -4,4 +4,16 @@ import static org.junit.Assert.*;
 
 public class PlayerTest {
 
+    public void testAddCard(){
+
+    }
+
+    public void testScore(){
+
+    }
+
+    public void testBusted(){
+
+    }
+
 }
