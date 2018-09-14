8d775034632bcced4c44b244b77bdc052c8e67d6
quanghienle <Hien1308>
2018-09-14 00:51:44 -0400

    updates on Deck class

--- src/main/java/simpleBlackJack/Deck.java
+++ src/main/java/simpleBlackJack/Deck.java
@@ -1,6 +1,49 @@
 package simpleBlackJack;
 
+import java.util.Random;
+import java.util.List;
+import java.util.LinkedList;
+import java.util.Collections;
+
+
 public class Deck {
 
+    private  Random rand = new Random();
+    private List<Card> deck;
 
+    //constructor
+    public Deck(){
+        //deck stores 52 cards as a linkedlist
+        this.deck = new LinkedList<Card>();
+
+        //initialize cards of a deck
+        for(int i=0; i< Card.Rank.length; i++ ) {
+            for (int k = 0; k < Card.Suit.length; k++) {
+                this.deck.add(new Card(Card.Suit[i]+ Card.Rank[k]));
+            }
+        }
+
+    }
+
+    //shuffle the deck
+    private void deckShuffle(){
+        //Shuffle the whole deck
+        Collections.shuffle(this.deck);
+    }
+
+    //get number of cards in the deck
+    private int getNumCards(){
+        return this.deck.size();
+    }
+
+    //get deck: list of cards
+    private List<Card> getDeck(){
+        return this.deck;
+    }
+
+    //withdraw the first card from the deck
+    private Card drawCard(){
+        //return the first card if deck has more than 0 cards
+        return getNumCards() != 0 ? this.deck.remove(0) : null;
+    }
 }
