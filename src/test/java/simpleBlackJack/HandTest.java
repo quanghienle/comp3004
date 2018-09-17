bd76e3b98a020a53496acf24a6b857bda6dfa371
quanghienle <Hien1308>
2018-09-16 21:35:25 -0400

    update on testing score. test the score in 3 circumstances: -A is 1 -A is 11 -A is both 1 and 11

--- src/test/java/simpleBlackJack/HandTest.java
+++ src/test/java/simpleBlackJack/HandTest.java
@@ -5,53 +5,71 @@ import static org.junit.Assert.*;
 public class HandTest {
 
     public void testAddCard(){
-        Hand user = new Hand();
+        Hand user = new Hand("Player");
         Card ex = new Card("DQ");
         user.addCard(ex);
 
-        assertTrue(user.getCard(0).equals(ex));
+        assertEquals(user.getCard(0), ex);
     }
 
     public void testScore(){
-        Hand user = new Hand();
+        Hand user1 = new Hand("Player");
+        Hand user2 = new Hand("Player");
+        Hand user3 = new Hand("Player");
+
 
         Card card_1 = new Card("DQ");
-        Card card_2 = new Card("H10");
+        Card card_2 = new Card("H8");
         Card card_3 = new Card("CA");
+        Card card_4 = new Card("DA");
 
-        user.addCard(card_1);
-        user.addCard(card_2);
+        //A as 11
+        user1.addCard(card_1);
+        user1.addCard(card_3);
 
-        assertEquals(20, user.getScore());
+        assertEquals(21, user1.getScore());
 
-        user.addCard(card_3);
-        //this ace will act as 1
-        assertEquals(21, user.getScore());
+        //A as 1
+        user2.addCard(card_1);
+        user2.addCard(card_2);
+        user2.addCard(card_3);
+
+        assertEquals(19, user1.getScore());
+
+        //A as 11 and 1
+        user3.addCard(card_1);
+        user3.addCard(card_2);
+        user3.addCard(card_3);
+        user3.addCard(card_4);
+
+        assertEquals(20, user1.getScore());
+
 
     }
 
     public void testBlackjack(){
-        Hand user = new Hand();
+        Hand user = new Hand("Player");
+        Hand user2 = new Hand("Player");
 
         Card card_1 = new Card("DQ");
         Card card_2 = new Card("H10");
         Card card_3 = new Card("CA");
 
+        //not a blackjack
         user.addCard(card_1);
         user.addCard(card_2);
 
-        assertFalse(user.blackJack());
+        assertFalse(user.hasBlackjack());
 
-        Hand user2 = new Hand();
-
+        ///has a blackjack
         user2.addCard(card_1);
         user2.addCard(card_3);
 
-        assertTrue(user2.blackJack());
+        assertTrue(user2.hasBlackjack());
     }
 
     public void testBusted(){
-        Hand user = new Hand();
+        Hand user = new Hand("Player");
 
         Card card_1 = new Card("DQ");
         Card card_2 = new Card("H10");
