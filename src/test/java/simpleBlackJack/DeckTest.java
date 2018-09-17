195ebdfecf048d0d0046f57850b51e73604ebb5b
quanghienle <Hien1308>
2018-09-16 22:02:04 -0400

    update on Deck test: -fixed test size of original deck and given deck

--- src/test/java/simpleBlackJack/DeckTest.java
+++ src/test/java/simpleBlackJack/DeckTest.java
@@ -1,20 +1,39 @@
 package simpleBlackJack;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class DeckTest {
 
     public void testNumCards(){
+
+        //the whole legit deck of cards has 52 cards
         Deck myDeck = new Deck();
         myDeck.generateWholeDeck();
         assertEquals(52, myDeck.getNumCards());
+
+        //deck with given cards
+        Deck myDeck2 = new Deck();
+        List<String> cards = new ArrayList<String>();
+        cards.add("H5");
+        cards.add("H2");
+        cards.add("H4");
+
+        myDeck2.generateGivenCards(cards);
+        assertEquals(3, myDeck2.getNumCards());
     }
 
     public void testShuffle(){
         Deck myDeck = new Deck();
-        String[] cards = {"H6", "CK", "D3"};
-        myDeck.generateGivenCards(cards);
+        List<String> cards = new ArrayList<String>();
 
+        cards.add("H6");
+        cards.add("CK");
+        cards.add("D3");
+
+        myDeck.generateGivenCards(cards);
 
 
         myDeck.deckShuffle();
@@ -22,18 +41,25 @@ public class DeckTest {
         Card card_2 = myDeck.getDeck().get(1);
         Card card_3 = myDeck.getDeck().get(2);
 
+        //test that they are not in the same order
         assertFalse(card_1.toString().equals("H6") && card_2.toString().equals("CK") && card_3.toString().equals("D3"));
 
     }
     public void testDrawCard(){
         Deck myDeck = new Deck();
-        String[] cards = {"H6", "CK", "D3"};
+        List<String> cards = new ArrayList<String>();
+
+        cards.add("H6");
+        cards.add("CK");
+        cards.add("D3");
+
         myDeck.generateGivenCards(cards);
+
 
         Card myCard = myDeck.drawCard();
 
         assertTrue(myCard.toString().equals("H6"));
-        assertEquals(51, myDeck.getNumCards());
+        assertTrue(myCard.toString().equals("CK"));
     }
 
 }
